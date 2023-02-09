b83cb0728dd7f5b6e6f546cb2a0d14cf56493e71
Akunmaru <augustineagena@gmail.com>
2023-02-09 09:06:41 -0700

    first commit

---
+++ AreaCalc.py
@@ -0,0 +1,8 @@
+print("Welcome to AREA calculator! \ninput the required values\nand I will handle the rest!")
+while True:
+    width = input("What is the width of the object? ")
+    if width.lower() == 'quit': break
+    length = input ("What is the length of the object? ")
+    if length.lower() == 'quit': break
+    Area = int(width) * int(length)
+    print(Area)
